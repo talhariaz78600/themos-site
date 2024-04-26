4dcbf64df3b626c9922c5d10385b97649d18a73d
Talha Riaz <muhammadtalha1005@gmail.com>
2024-04-26 17:18:37 +0500

    background

--- src/components/home/PageHome.tsx
+++ src/components/home/PageHome.tsx
@@ -21,7 +21,7 @@ function PageHome() {
                     </div>
 
                     <SectionOurFeatures />
-                    <div>
+                    <div  className="relative py-16">
                     <div className="nc-BackgroundSection absolute inset-y-0 w-screen xl:max-w-[1340px] 2xl:max-w-screen-2xl left-1/2 transform -translate-x-1/2 xl:rounded-[40px] z-0 bg-black bg-opacity-20 " data-nc-id="BackgroundSection"></div>
                     <SectionHowItWork />
                     </div>
