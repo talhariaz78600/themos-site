43ca657a7add7dd0c21eae50f0a62ec820c13b0d
Talha Riaz <muhammadtalha1005@gmail.com>
2024-05-11 17:48:29 +0500

    welldone

--- src/app/vehicle-selection/page.tsx
+++ src/app/vehicle-selection/page.tsx
@@ -7,8 +7,7 @@ const page =async () => {
                 method: "GET",
                 headers: {
                   "Content-Type": "application/json"
-                },
-                cache: 'force-cache'
+                }
               });
               const response=await res.json();
               // console.log(response)
